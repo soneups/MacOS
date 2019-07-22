37afa515ddbfa98651780c350a87b8487d603e78
Gary Sone <gary.sone@bh.sone.net>
2019-07-22 15:46:25 +0100

    Create image.sh
    Initial script

---
+++ image.sh
@@ -0,0 +1,7 @@
+# alt + 3 = # ' how to get a # on a mac keyboard!
+
+#this will create the Mojave installer 1) assumes you have downloaded it from the app store 2) your usb stick is called '16GB_USB'
+#sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/createinstallmedia --volume /Volumes/16GB_USB/
+
+#this command line is the big red button (BRB) and will effectively factory-reset your mac! - HT: Jamf...
+#/Applications/Install macOS Mojave.app/Contents/Resources/startosinstall —erase install —newvolume “MacHD” —agreetolicese
